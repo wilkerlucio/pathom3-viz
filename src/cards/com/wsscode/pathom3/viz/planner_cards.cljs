47a1d1072e3d43bed44c3a7bd333b80b9bede509
Wilker Lucio <wilkerlucio@gmail.com>
2020-11-12 11:06:00 -0300

    Lint fixes

--- src/cards/com/wsscode/pathom3/viz/planner_cards.cljs
+++ src/cards/com/wsscode/pathom3/viz/planner_cards.cljs
@@ -1,35 +1,24 @@
 (ns com.wsscode.pathom3.viz.planner-cards
   (:require
-    [cljs.tools.reader :refer [read-string]]
     [com.fulcrologic.guardrails.core :refer [<- => >def >defn >fdef ? |]]
+    [com.wsscode.pathom3.connect.indexes :as pci]
     [com.wsscode.pathom3.viz.plan :as viz-plan]
     [edn-query-language.core :as eql]
-    [com.wsscode.pathom3.connect.planner :as pcp]
-    [com.wsscode.pathom3.connect.indexes :as pci]
     [helix.core :refer [$]]
     [nubank.workspaces.card-types.react :as ct.react]
     [nubank.workspaces.core :as ws]
-    [nubank.workspaces.model :as wsm]
-    [helix.hooks :as hooks]))
+    [nubank.workspaces.model :as wsm]))
 
 (ws/defcard plan-view-cytoscape-card
-  {::wsm/align ::wsm/stretch-flex}
-  (ct.react/react-card
-    (let [source-state (hooks/use-state '{::pci/index-oir      {:a {#{} #{a}}
-                                                                :b {#{:g} #{b}}
-                                                                :c {#{} #{c}}
-                                                                :f {#{:e} #{f}}
-                                                                :g {#{:c :f} #{g}}
-                                                                :h {#{:a :b} #{h}}}
-                                          ::pcp/available-data {}
-                                          ::eql/query          [:h]})]
-      ($ viz-plan/PlanCytoscape
-        {:frames
-         (->> (viz-plan/compute-frames
-                '{::pci/index-oir {:a {#{:c :b} #{a}}
-                                   :b {#{:d} #{bc bc2}}
-                                   :c {#{:d} #{bc bc2}}
-                                   :e {#{:d} #{bc bc2}}
-                                   :d {#{} #{d}}}
-                  ::eql/query     [:a]})
-              (mapv (juxt identity viz-plan/c-nodes-edges)))}))))
+            {::wsm/align ::wsm/stretch-flex}
+            (ct.react/react-card
+              ($ viz-plan/PlanCytoscape
+                 {:frames
+                  (->> (viz-plan/compute-frames
+                         '{::pci/index-oir {:a {#{:c :b} #{a}}
+                                            :b {#{:d} #{bc bc2}}
+                                            :c {#{:d} #{bc bc2}}
+                                            :e {#{:d} #{bc bc2}}
+                                            :d {#{} #{d}}}
+                           ::eql/query     [:a]})
+                       (mapv (juxt identity viz-plan/c-nodes-edges)))})))
