a001e2bfa1afe169fdc81b132c6f0ab4d476c6f5
Wilker Lucio <wilkerlucio@gmail.com>
2020-11-12 11:06:30 -0300

    Format update

--- src/main/com/wsscode/pathom3/viz/plan.cljs
+++ src/main/com/wsscode/pathom3/viz/plan.cljs
@@ -123,104 +123,104 @@
   [{:keys [container-ref display-type]} elements]
   (let [cy-ref (hooks/use-ref nil)]
     (hooks/use-effect [display-type]
-                      (some-> @cy-ref
-                              (.style)
-                              (.selector "node")
-                              (.style "label" (display-type->label display-type))
-                              (.update)))
+      (some-> @cy-ref
+              (.style)
+              (.selector "node")
+              (.style "label" (display-type->label display-type))
+              (.update)))
     (hooks/use-effect [elements]
-                      (if @cy-ref
-                        (let [cy         ^js @cy-ref
-                              {:strs [nodes edges]} (group-by :group elements)
-                              [add-nodes remove-nodes] (node-diff cy nodes (.nodes cy))
-                              [add-edges remove-edges] (node-diff cy edges (.edges cy))
-                              remove-all (.add remove-nodes remove-edges)]
-                          (.batch cy
-                                  (fn []
-                                    (remove-fade-out cy remove-all)
-                                    (doseq [{:keys [data classes]} nodes]
-                                      (when-let [node ^js (first (.nodes cy (str "[id=\"" (:id data) "\"]")))]
-                                        (.classes node (into-array classes))))
-                                    (add-fade-in cy (clj->js add-nodes))
-                                    (add-fade-in cy (clj->js add-edges))))
-                          (-> cy
-                              (.elements)
-                              (.difference remove-all)
-                              (.layout #js {:name "dagre" :rankDir "LR" :animate true :animationDuration anim-duration})
-                              (.run)))
-                        (reset! cy-ref
-                                (cytoscape
-                                  #js {:container     @container-ref
-                                       :autoungrabify true
-                                       :layout        #js {:name    "dagre"
-                                                           :rankDir "LR"}
-                                       :style         #js [#js {:selector "node"
-                                                                :style    #js {:content             (display-type->label display-type)
-                                                                               :text-valign         "center"
-                                                                               :transition-property "border-color border-width"
-                                                                               :transition-duration (str anim-duration "ms")}}
-                                                           #js {:selector "node.node-and"
-                                                                :style    #js {:background-color "#bcbd22"}}
-                                                           #js {:selector "node.node-or"
-                                                                :style    #js {:background-color "#17becf"}}
-                                                           #js {:selector "node.node-resolver"
-                                                                :style    #js {:background-color "#7f7f7f"}}
-                                                           #js {:selector "node.root"
-                                                                :style    #js {:border-width 3
-                                                                               :border-color "#000"}}
-                                                           #js {:selector "edge"
-                                                                :style    #js {:curve-style        "bezier"
-                                                                               :width              2
-                                                                               :arrow-scale        0.8
-                                                                               :target-arrow-shape "triangle"}}
-                                                           #js {:selector "edge.branch"
-                                                                :style    #js {:line-color         "#ff7f0e"
-                                                                               :target-arrow-color "#ff7f0e"}}
-                                                           #js {:selector "edge.next"
-                                                                :style    #js {:line-color         "#000"
-                                                                               :target-arrow-color "#000"}}]
-                                       :elements      (clj->js elements)}))))))
+      (if @cy-ref
+        (let [cy         ^js @cy-ref
+              {:strs [nodes edges]} (group-by :group elements)
+              [add-nodes remove-nodes] (node-diff cy nodes (.nodes cy))
+              [add-edges remove-edges] (node-diff cy edges (.edges cy))
+              remove-all (.add remove-nodes remove-edges)]
+          (.batch cy
+                  (fn []
+                    (remove-fade-out cy remove-all)
+                    (doseq [{:keys [data classes]} nodes]
+                      (when-let [node ^js (first (.nodes cy (str "[id=\"" (:id data) "\"]")))]
+                        (.classes node (into-array classes))))
+                    (add-fade-in cy (clj->js add-nodes))
+                    (add-fade-in cy (clj->js add-edges))))
+          (-> cy
+              (.elements)
+              (.difference remove-all)
+              (.layout #js {:name "dagre" :rankDir "LR" :animate true :animationDuration anim-duration})
+              (.run)))
+        (reset! cy-ref
+                (cytoscape
+                  #js {:container     @container-ref
+                       :autoungrabify true
+                       :layout        #js {:name    "dagre"
+                                           :rankDir "LR"}
+                       :style         #js [#js {:selector "node"
+                                                :style    #js {:content             (display-type->label display-type)
+                                                               :text-valign         "center"
+                                                               :transition-property "border-color border-width"
+                                                               :transition-duration (str anim-duration "ms")}}
+                                           #js {:selector "node.node-and"
+                                                :style    #js {:background-color "#bcbd22"}}
+                                           #js {:selector "node.node-or"
+                                                :style    #js {:background-color "#17becf"}}
+                                           #js {:selector "node.node-resolver"
+                                                :style    #js {:background-color "#7f7f7f"}}
+                                           #js {:selector "node.root"
+                                                :style    #js {:border-width 3
+                                                               :border-color "#000"}}
+                                           #js {:selector "edge"
+                                                :style    #js {:curve-style        "bezier"
+                                                               :width              2
+                                                               :arrow-scale        0.8
+                                                               :target-arrow-shape "triangle"}}
+                                           #js {:selector "edge.branch"
+                                                :style    #js {:line-color         "#ff7f0e"
+                                                               :target-arrow-color "#ff7f0e"}}
+                                           #js {:selector "edge.next"
+                                                :style    #js {:line-color         "#000"
+                                                               :target-arrow-color "#000"}}]
+                       :elements      (clj->js elements)}))))))
 
 (h/defnc ^:export PlanCytoscape [{:keys [frames]}]
-         (let [[current-frame :as frame-state] (hooks/use-state (dec (count frames)))
-               [{::pcp/keys [snapshot-message]} elements] (get frames current-frame)
-               [display-type :as display-type-state] (hooks/use-state ::display-type-node-id)
-               [show-history? set-show-history] (ui/use-persistent-state ::show-history? true)
-               container-ref (hooks/use-ref nil)]
-           (cytoscape-planner-effect {:container-ref container-ref
-                                      :display-type  display-type} elements)
-           (dom/div {:style {:width            "100%"
-                             :height           "100%"
-                             :display          "flex"
-                             :flex-direction   "column"
-                             :background-color "#eee"
-                             :color            "#000"}}
-                    (dom/div {:style {:display "flex"
-                                      :align-items "center"}}
-                             (dom/button {:onClick #(set-show-history (not show-history?))}
-                                         (if show-history? "Hide History" "Show History"))
-                             (ui/dom-select {::ui/options [[::display-type-node-id "Node ID"]
-                                                           [::display-type-label "Label"]]
-                                             ::ui/state   display-type-state})
-                             (dom/div (str snapshot-message)))
-                    (dom/div {:style {:flex     "1"
-                                      :overflow "hidden"
-                                      :display  "flex"}}
-                             (if show-history?
-                               (dom/select {:style {:width "160px"} :size 2
-                                            :&     (ui/dom-props {::ui/state (ui/state-hook-serialize frame-state)})}
-                                           (for [[i {::pcp/keys [snapshot-message]}] (->> frames (map first) (map vector (range)))]
-                                             (dom/option {:key i :value i} snapshot-message))))
-                             (dom/div {:style {:flex     "1"
-                                               :overflow "hidden"}
-                                       :ref   container-ref})))))
+  (let [[current-frame :as frame-state] (hooks/use-state (dec (count frames)))
+        [{::pcp/keys [snapshot-message]} elements] (get frames current-frame)
+        [display-type :as display-type-state] (hooks/use-state ::display-type-node-id)
+        [show-history? set-show-history] (ui/use-persistent-state ::show-history? true)
+        container-ref (hooks/use-ref nil)]
+    (cytoscape-planner-effect {:container-ref container-ref
+                               :display-type  display-type} elements)
+    (dom/div {:style {:width            "100%"
+                      :height           "100%"
+                      :display          "flex"
+                      :flex-direction   "column"
+                      :background-color "#eee"
+                      :color            "#000"}}
+      (dom/div {:style {:display     "flex"
+                        :align-items "center"}}
+        (dom/button {:onClick #(set-show-history (not show-history?))}
+          (if show-history? "Hide History" "Show History"))
+        (ui/dom-select {::ui/options [[::display-type-node-id "Node ID"]
+                                      [::display-type-label "Label"]]
+                        ::ui/state   display-type-state})
+        (dom/div (str snapshot-message)))
+      (dom/div {:style {:flex     "1"
+                        :overflow "hidden"
+                        :display  "flex"}}
+        (if show-history?
+          (dom/select {:style {:width "160px"} :size 2
+                       :&     (ui/dom-props {::ui/state (ui/state-hook-serialize frame-state)})}
+            (for [[i {::pcp/keys [snapshot-message]}] (->> frames (map first) (map vector (range)))]
+              (dom/option {:key i :value i} snapshot-message))))
+        (dom/div {:style {:flex     "1"
+                          :overflow "hidden"}
+                  :ref   container-ref})))))
 
 (h/defnc ^:export PlanCytoscapeJS [{:keys [oir query]}]
-         ($ PlanCytoscape
-            {:frames
-             (->> (compute-frames {::pci/index-oir (read-string oir)
-                                   ::eql/query     (read-string query)})
-                  (mapv (juxt identity c-nodes-edges)))}))
+  ($ PlanCytoscape
+     {:frames
+      (->> (compute-frames {::pci/index-oir (read-string oir)
+                            ::eql/query     (read-string query)})
+           (mapv (juxt identity c-nodes-edges)))}))
 
 (comment
   (gobj/equals #js {:foo #js ["bar"]} #js {:foo #js ["bar"]})
